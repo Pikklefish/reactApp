f3cf6f37dfcd3de62a851dfe8bc5c2c870226f9e
Peter.S <nsaspetersong@gmail.com>
2023-12-11 22:34:26 -0500

    push

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,32 @@
+import { MouseEvent } from "react";
+
+
+
 function ListGroup() {
+  const items = ["New York", "Seoul", "London", "Paris"];
+  let selectedIndex = -1;
+
+//event handler
+const handleClick = (event: MouseEvent) => console.log(event);
+
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      {/* If condition true go to second eleemnt after &&, if condition flase return false  */}
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className="list-group-item active"
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
