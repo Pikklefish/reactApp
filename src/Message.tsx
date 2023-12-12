b373a9b669b5f8c1e70c0b1d74f01a73f35e41e9
Peter.S <nsaspetersong@gmail.com>
2023-12-11 20:38:07 -0500

    List Group

--- src/Message.tsx
+++ src/Message.tsx
@@ -2,8 +2,12 @@
 //PascalCasing (naming convention)
 function Message(){
     // JSX: JavaScript XML
+
+    const name ='Peter';
+    if (name)
+        return <h1>Hello {name}</h1>;
     
-    return <h1>Hello World</h1>;
+    return <h1>Hello World</h1>
 }
 
 export default Message;
